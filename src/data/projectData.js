0cee3c92d2f23458708ff40a9bcc2bf670e39c2d
Diwas Sapkota <diwassapkota805@gmail.com>
2024-01-31 00:06:59 -0600

    made modificfication in project

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,29 +1,29 @@
 import project1 from "../assets/portfolio.png";
-import project2 from "../assets/fileSystem.png";
+import project2 from "../assets/tourEZ.png";
 import project3 from "../assets/hangingManny.png";
 import project4 from "../assets/javaGame.png";
+import project5 from "../assets/fileSystem.png";
+
 let projects = [
     {
         id: 1,
         title: "portfolio",
         description: "This is my portfolio website. I built it using React.",
         github_link: "https://github.com/diwassapkota805/Personal_Website",
-        image: project1, // put image here
+        image: project1,
     },
     {
         id: 2,
-        title: "File System",
-        description: "This is a User-space file system with 226 bytes of drive space for efficient file management within a disk image. I built it using C",
-        github_link: "https://github.com/CSE3320-Spring23/filesystem-four-loopers",
-        // put an image from assent folder
+        title: "TourEZ",
+        description: "This is a full stack web application to help people plan thier trips. I built it using React, Node.js, Express.js, and MongoDB.",
+        github_link: "",
         image: project2,
     },
     {
         id: 3,
         title: "HangingManny",
-        description: "This is a full stack 2d android game. I built it using C# and Unity.",
+        description: "This is a full stack 2d android game. I built it using C#.",
         github_link: "https://github.com/diwassapkota805/HangingManny",
-        // put an image from assent folder
         image: project3,
     },
     {
@@ -31,16 +31,14 @@ let projects = [
         title: "Java Game",
         description: "This is a 2d combat game. I built it using Java.",
         github_link: "https://github.com/diwassapkota805/JavaGame",
-        // put an image from assent folder
         image: project4,
     },
     {
-        id: 2,
+        id: 5,
         title: "File System",
-        description: "This is my portfolio website. I built it using React and Material UI.",
+        description: "This is my portfolio website. I built it using React.",
         github_link: "https://github.com/diwassapkota805/Personal_Website",
-        // put an image from assent folder
-        image: project2,
+        image: project5,
     },
 
 ]
--- src/data/timelineElements.js
+++ src/data/timelineElements.js
@@ -3,7 +3,7 @@ let timelineElements = [
         id: 1,
         title: "Web Developer",
         location: "Remote",
-        description: "I am currently working as a web developer for a company in the US. I am responsible for developing and maintaining the company's website.",
+        description: "I was responsible for contributing to campaign planning, conducting ongoing website analysis, and generating reports to enhance user engagement, online visibility, and improve website performance and user experience.",
         date: "September 2023 - November 2023",
         icon: "work",
     },
@@ -11,7 +11,7 @@ let timelineElements = [
         id: 2,
         title: "Terminal Supervisor",
         location: "DFW Airport",
-        description: "I was responsible for managing the day to day operations of the terminal. I was also responsible for managing the staff and ensuring that the terminal was running smoothly.",
+        description: "I was responsible for leading a team of assistance providers, ensuring efficient support for 50+ daily passengers, and collaborating with stakeholders to optimize wheelchair assistance processes.",
         date: "June 2021 - June 2023",
         icon: "work",
     }
